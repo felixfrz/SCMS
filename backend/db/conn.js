50f87f91c20e8a1c6d9852c47b6cbbd7edeb136a
Ayodeji Oladoyin <frz@Ayodejis-MacBook-Pro.local>
2023-07-06 16:53:04 +0100

    deploy

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,12 +2,12 @@ const mysql = require("mysql");
 
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "scms"
+	host: 'us-cdbr-east-06.cleardb.net',
+	user: 'beffa1464bd0ca',
+	password: '7e8041f3',
+	database: 'heroku_a6cf754832742b5',
 });
-
+// mysql://beffa1464bd0ca:7e8041f3@us-cdbr-east-06.cleardb.net/heroku_a6cf754832742b5?reconnect=true
 conn.connect((err)=>{
     if(err) throw err;
     console.log("MySQL Connected!");
